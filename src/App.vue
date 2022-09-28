f380b32477278da6f439378aaf64a4b8b697b2d4
Francesco Gentile <45791472+FrancescoGentileDev@users.noreply.github.com>
2022-09-28 15:14:32 +0200

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <div id="app">
+    HELLO WORLD!
+  </div>
+</template>
+
+<script>
+
+
+export default {
+  name: 'App',
+  components: {
+
+  }
+}
+</script>
+
+<style lang="scss">
+@import "/node_modules/bootstrap/scss/bootstrap.scss";
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+}
+</style>
