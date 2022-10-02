7229d53e90e013fd49f4be518ec75929ba59c2b1
svalvastic <assogames2@gmail.com>
2022-10-03 00:27:51 +0200

    ✨ feat(header): return home button

--- src/components/headerSection.vue
+++ src/components/headerSection.vue
@@ -2,12 +2,12 @@
   <div class="d-flex head p-2 px-4 justify-content-between">
     <div class="d-flex">
       <div class="logo w-25">
-        <img class="w-100" src="@/assets/netflix_logo.png" alt="" srcset="" />
+        <img class="w-100" src="@/assets/netflix_logo.png" alt="" srcset="" @click="returnHome"/>
       </div>
 
       <div class="link">
         <ul>
-          <li><a class="active" href="#">Home</a></li>
+          <li><a class="active" href="#"  @click="returnHome" >Home</a></li>
           <li><a href="#">Tv Shows</a></li>
           <li><a href="#">Movies</a></li>
           <li><a href="#">New & popular</a></li>
@@ -26,13 +26,13 @@
         v-model="textInput"
         @keyup.enter="entered"
       />
-      <font-awesome-icon class="fs-4" icon="fa-solid fa-magnifying-glass" />
+      <font-awesome-icon class="fs-4 magnifying-glass" icon="fa-solid fa-magnifying-glass" @click="entered"/>
       <div class="dvd">
       <a href="">DVD</a>
       </div>
       <font-awesome-icon class="fs-3" icon="fa-solid fa-bell" />
       <div class="profile-pic d-flex align-items-center">
-      <img class="w-100" src="@/assets/profile_pic1.png" alt="">
+      <img class="w-100" :src="profile" alt="">
       <font-awesome-icon class="ms-2" icon="fa-solid fa-caret-down" />
       </div>
     </div>
@@ -46,10 +46,21 @@ export default {
       textInput: "",
     };
   },
+  props: {
+    profile: String,
+
+  },
+
   methods: {
     entered() {
       this.$emit("text", this.textInput);
     },
+    returnHome() {
+      if(this.textInput!== "") {
+        this.textInput = ""
+        this.entered()
+        }
+    }
   },
 };
 </script>
@@ -64,6 +75,8 @@ export default {
 
   .logo {
     max-width: 200px;
+    user-select: none;
+    cursor: pointer;
   }
    a {
           text-decoration: none;
@@ -81,13 +94,19 @@ export default {
       display: flex;
       gap: 1rem;
       margin: 0;
-      li {
 
-      }
     }
   }
   .rightHead {
-    gap: 2rem;
+gap: 2rem;
+    input {
+      background-color: $backround-primary;
+      color: white;
+    }
+    .magnifying-glass {
+      cursor: pointer;
+
+    }
     .profile-pic {
       max-width: 45px;
       img {
