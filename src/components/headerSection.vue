d563b0fb5d41714f6ade7ad89945f98a0d2be850
svalvastic <assogames2@gmail.com>
2022-10-02 18:53:27 +0200

    ✨ feat(header): styled header

--- src/components/headerSection.vue
+++ src/components/headerSection.vue
@@ -1,10 +1,24 @@
 <template>
-  <div class="row head">
-    <div class="mb-3 col-2">
-      <label for="" class="form-label">Search Film</label>
+  <div class="d-flex head p-2 px-4 justify-content-between">
+    <div class="d-flex">
+      <div class="logo w-25">
+        <img class="w-100" src="@/assets/netflix_logo.png" alt="" srcset="" />
+      </div>
+
+      <div class="link">
+        <ul>
+          <li><a class="active" href="#">Home</a></li>
+          <li><a href="#">Tv Shows</a></li>
+          <li><a href="#">Movies</a></li>
+          <li><a href="#">New & popular</a></li>
+          <li><a href="#">My List</a></li>
+        </ul>
+      </div>
+    </div>
+    <div class="d-flex align-items-center rightHead me-4">
       <input
         type="text"
-        class="form-control"
+        class="form-control my-4"
         name=""
         id=""
         aria-describedby="helpId"
@@ -12,23 +26,31 @@
         v-model="textInput"
         @keyup.enter="entered"
       />
+      <font-awesome-icon class="fs-4" icon="fa-solid fa-magnifying-glass" />
+      <div class="dvd">
+      <a href="">DVD</a>
+      </div>
+      <font-awesome-icon class="fs-3" icon="fa-solid fa-bell" />
+      <div class="profile-pic d-flex align-items-center">
+      <img class="w-100" src="@/assets/profile_pic1.png" alt="">
+      <font-awesome-icon class="ms-2" icon="fa-solid fa-caret-down" />
+      </div>
     </div>
   </div>
 </template>
 
 <script>
 export default {
-data() {
+  data() {
     return {
-        textInput: ""
-    }
-},
-methods: {
+      textInput: "",
+    };
+  },
+  methods: {
     entered() {
-        this.$emit("text", this.textInput)
-    }
-}
-
+      this.$emit("text", this.textInput);
+    },
+  },
 };
 </script>
 
@@ -39,6 +61,39 @@ methods: {
   z-index: 100000;
   width: 101%;
   background-color: rgba($backround-primary, $alpha: 0.7);
-  
+
+  .logo {
+    max-width: 200px;
+  }
+   a {
+          text-decoration: none;
+          color: $head-text-color;
+          &.active {
+            color: $white-color;
+          }
+        }
+  .link {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    ul {
+      list-style: none;
+      display: flex;
+      gap: 1rem;
+      margin: 0;
+      li {
+
+      }
+    }
+  }
+  .rightHead {
+    gap: 2rem;
+    .profile-pic {
+      max-width: 45px;
+      img {
+      border-radius: 10px;
+      }
+    }
+  }
 }
 </style>
--- src/components/videoSection.vue
+++ src/components/videoSection.vue
@@ -4,10 +4,30 @@
       <video autoplay muted loop>
         <source src="../assets/stream.mp4" />
       </video>
-      
+
       <div class="blockHover">
-      <div class="rick-container">BLLBLLVBLV</div>
       </div>
+        <div class="rick-container">
+          <img class="w-100" src="@/assets/Rick_and_Morty.svg" alt="" srcset="" />
+          <div class="details">
+            <div class="description">
+              <div class="title-line">
+                Lorem ipsum dolor sit, amet consectetur adipisicing elit. Reprehenderit ducimus,
+                tempore fugit
+              </div>
+            </div>
+            <div class="buttons">
+              <button>
+                <font-awesome-icon class="play" icon="fa-solid fa-play" />
+                Play
+              </button>
+              <button class="darker d-flex">
+                <font-awesome-icon icon="fa-solid fa-info" />
+                <div class="mb-1">More Info</div>
+              </button>
+            </div>
+          </div>
+        </div>
     </div>
   </div>
 </template>
@@ -39,8 +59,6 @@ export default {
 
 <style lang="scss" scoped>
 @import "@/variables";
-.background {
-}
 
 video {
   position: absolute;
@@ -68,5 +86,58 @@ video {
   width: 100%;
   height: 150%;
   background-image: linear-gradient(180deg, transparent, $backround-primary);
+}
+
+.rick-container {
+  margin-top: 100px;
+  height: 80%;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  margin-left: 7rem;
+  max-width: 800px;
+  .details {
+    margin-left: 2rem;
+    .title-line {
+      margin-top: 2rem;
+      font-size: 1.8rem;
+    }
+    .buttons {
+      display: flex;
+      gap: 2rem;
+      margin-top: 1.5rem;
+      button {
+        position: relative;
+        z-index: 10000000000000;
+        .play {
+          color: black;
+        }
+        background-color: $white-color;
+        color: $backround-secondary;
+        padding: 0.7rem 3rem;
+        padding-left: 1.5rem;
+        border-radius: 0.7rem;
+        font-size: 1.6rem;
+        font-weight: bold;
+        border: none;
+        &.darker {
+          gap: 0.5rem;
+          background-color:rgba($head-text-color, .8);
+          color: $white-color;
+          
+        }
+        &:hover {
+          background-color:$head-text-color
+        }
+      }
+    }
+  }
+}
+
+.fa-info {
+  aspect-ratio: 1/1;
+  padding: 0.2rem;
+  border: 3px solid $white-color;
+  border-radius: 50%;
 }
 </style>
