80a0e5c3c17d883b4ca3d4ed7a00a9509f4f5b92
svalvastic <assogames2@gmail.com>
2022-10-03 14:39:17 +0200

    📃 docs(docs): add docs

--- src/call.js
+++ src/call.js
@@ -17,25 +17,43 @@ class Call {
     },
   };
 
+  /**
+   * 
+   * @param {Object} queries Parametri della query da aggiungere alla richiesta
+   */
   constructor(queries) {
     for (let key in queries) {
       this.data.params[key] = queries[key];
     }
   }
+    /**
+   * 
+   * @param {Object} queries chiavi e valori da aggiungere alla query
+   */
   addQuery(queries) {
     for (let key in queries) {
       this.data.params[key] = queries[key];
     }
   }
+  /**
+   * 
+   * @param {Object} queries chiavi e valori da rimuovere dalla query
+   */
   removeQuery(queries) {
     for (let key in queries) {
       delete this.data.params[key];
     }
   }
+
   get Params() {
     return this.data;
   }
 
+/**
+ *  EFFETTUA UNA CHIAMATA AXIOS SU GLI ENDPOINT PASSATI
+ * @param  {...any} path Enpoints
+ * @returns {Object} dati richiesti
+ */
   async makeCall(...path) {
     let string = this.path;
 
@@ -56,14 +74,20 @@ class Call {
 
     return response;
   }
-
-  async moreInformationAllMulti(response) {
+  /**
+   * 
+   * @param {Object[]} tvShows Array di serie tv al quale chiedere più info
+   * @returns {Object[]} Ritorna un array simile a quello in ingresso con
+   * nuove proprietà contenti informazioni più dettagliate:
+   *  
+   */
+  async moreInformationAllMulti(moviesOrSeries) {
     const response_1 = new Promise(async (resolve) => {
-      response.results = response.results.filter((value, index) => {
+      moviesOrSeries.results = moviesOrSeries.results.filter((value, index) => {
         if (value.media_type !== "person") return true;
       });
 
-      response.results.forEach(async (value, index) => {
+      moviesOrSeries.results.forEach(async (value, index) => {
         // console.log(value)
         let info = await this.getMoreInformation(value.id, value.media_type);
         if (value.media_type === "movie" || this.type === "movie") {
@@ -78,18 +102,18 @@ class Call {
 
           if (production_countries.length === 0) production_countries.push({ iso_3166_1: "US" });
           if (value.poster_path !== null || backdrop_path !== null) {
-            response.results[index].image = this.getImage(index, 5, value.poster_path);
-            response.results[index].backdrop = this.getImage(index, 5, backdrop_path);
+            moviesOrSeries.results[index].image = this.getImage(index, 5, value.poster_path);
+            moviesOrSeries.results[index].backdrop = this.getImage(index, 5, backdrop_path);
           } else {
-            response.results[index].image =
+            moviesOrSeries.results[index].image =
               "https://upload.wikimedia.org/wikipedia/commons/b/b9/No_Cover.jpg";
-            response.results[index].backdrop =
+            moviesOrSeries.results[index].backdrop =
               "https://upload.wikimedia.org/wikipedia/commons/b/b9/No_Cover.jpg";
           }
-          response.results[index].flag = this.getFlag(production_countries[0].iso_3166_1);
-          response.results[index].languageCode = value.original_language;
-          response.results[index].original_language = codeToLanguage[value.original_language];
-          response.results[index] = {
+          moviesOrSeries.results[index].flag = this.getFlag(production_countries[0].iso_3166_1);
+          moviesOrSeries.results[index].languageCode = value.original_language;
+          moviesOrSeries.results[index].original_language = codeToLanguage[value.original_language];
+          moviesOrSeries.results[index] = {
             ...value,
             backdrop_path,
             genres,
@@ -109,21 +133,21 @@ class Call {
           } = info;
 
           if (value.poster_path !== null || backdrop_path !== null) {
-            response.results[index].image = this.getImage(index, 5, value.poster_path);
-            response.results[index].backdrop = this.getImage(index, 5, backdrop_path);
+            moviesOrSeries.results[index].image = this.getImage(index, 5, value.poster_path);
+            moviesOrSeries.results[index].backdrop = this.getImage(index, 5, backdrop_path);
           } else {
-            response.results[index].image =
+            moviesOrSeries.results[index].image =
               "https://upload.wikimedia.org/wikipedia/commons/b/b9/No_Cover.jpg";
-            response.results[index].backdrop =
+            moviesOrSeries.results[index].backdrop =
               "https://upload.wikimedia.org/wikipedia/commons/b/b9/No_Cover.jpg";
           }
 
-          response.results[index].flag = this.getFlag(origin_country[0]);
-          response.results[index].languageCode = value.original_language;
-          response.results[index].original_language = codeToLanguage[value.original_language];
+          moviesOrSeries.results[index].flag = this.getFlag(origin_country[0]);
+          moviesOrSeries.results[index].languageCode = value.original_language;
+          moviesOrSeries.results[index].original_language = codeToLanguage[value.original_language];
           seasons = seasons.length;
 
-          response.results[index] = {
+          moviesOrSeries.results[index] = {
             title: value.name,
             release_date: first_air_date,
             backdrop_path,
@@ -137,15 +161,21 @@ class Call {
         }
       });
       // console.log("api", response)
-      this.results = response;
-      resolve(response);
+      this.results = moviesOrSeries;
+      resolve(moviesOrSeries);
     });
     return response_1;
   }
-
-  async moreInformationAllMovie(response) {
+  /**
+   * 
+   * @param {Object[]} tvShows Array di Film al quale chiedere più info
+   * @returns {Object[]} Ritorna un array simile a quello in ingresso con
+   * nuove proprietà contenti informazioni più dettagliate:
+   *  
+   */
+  async moreInformationAllMovie(movies) {
     const response_1 = new Promise(async (resolve) => {
-      response.results.forEach(async (value, index) => {
+      movies.results.forEach(async (value, index) => {
         let info = await this.getMoreInformation(value.id);
 
         let {
@@ -159,18 +189,18 @@ class Call {
 
         if (production_countries.length === 0) production_countries.push({ iso_3166_1: "US" });
         if (value.poster_path !== null || backdrop_path !== null) {
-          response.results[index].image = this.getImage(index, 5, value.poster_path);
-          response.results[index].backdrop = this.getImage(index, 5, backdrop_path);
+          movies.results[index].image = this.getImage(index, 5, value.poster_path);
+          movies.results[index].backdrop = this.getImage(index, 5, backdrop_path);
         } else {
-          response.results[index].image =
+          movies.results[index].image =
             "https://upload.wikimedia.org/wikipedia/commons/b/b9/No_Cover.jpg";
-          response.results[index].backdrop =
+          movies.results[index].backdrop =
             "https://upload.wikimedia.org/wikipedia/commons/b/b9/No_Cover.jpg";
         }
-        response.results[index].flag = this.getFlag(production_countries[0].iso_3166_1);
-        response.results[index].languageCode = value.original_language;
-        response.results[index].original_language = codeToLanguage[value.original_language];
-        response.results[index] = {
+        movies.results[index].flag = this.getFlag(production_countries[0].iso_3166_1);
+        movies.results[index].languageCode = value.original_language;
+        movies.results[index].original_language = codeToLanguage[value.original_language];
+        movies.results[index] = {
           ...value,
           backdrop_path,
           genres,
@@ -180,15 +210,22 @@ class Call {
           belongs_to_collection,
         };
       });
-      this.results = response;
-      resolve(response);
+      this.results = movies;
+      resolve(movies);
     });
     return response_1;
   }
 
-  async moreInformationAllTV(response) {
+  /**
+   * 
+   * @param {Object[]} tvShows Array di serie tv al quale chiedere più info
+   * @returns {Object[]} Ritorna un array simile a quello in ingresso con
+   * nuove proprietà contenti informazioni più dettagliate:
+   *  
+   */
+  async moreInformationAllTV(tvShows) {
     const response_1 = new Promise(async (resolve) => {
-      response.results.forEach(async (value, index) => {
+      tvShows.results.forEach(async (value, index) => {
         let info = await this.getMoreInformation(value.id);
         let {
           backdrop_path,
@@ -200,21 +237,21 @@ class Call {
         } = info;
 
         if (value.poster_path !== null || backdrop_path !== null) {
-          response.results[index].image = this.getImage(index, 5, value.poster_path);
-          response.results[index].backdrop = this.getImage(index, 5, backdrop_path);
+          tvShows.results[index].image = this.getImage(index, 5, value.poster_path);
+          tvShows.results[index].backdrop = this.getImage(index, 5, backdrop_path);
         } else {
-          response.results[index].image =
+          tvShows.results[index].image =
             "https://upload.wikimedia.org/wikipedia/commons/b/b9/No_Cover.jpg";
-          response.results[index].backdrop =
+          tvShows.results[index].backdrop =
             "https://upload.wikimedia.org/wikipedia/commons/b/b9/No_Cover.jpg";
         }
 
-        response.results[index].flag = this.getFlag(origin_country[0]);
-        response.results[index].languageCode = value.original_language;
-        response.results[index].original_language = codeToLanguage[value.original_language];
+        tvShows.results[index].flag = this.getFlag(origin_country[0]);
+        tvShows.results[index].languageCode = value.original_language;
+        tvShows.results[index].original_language = codeToLanguage[value.original_language];
         seasons = seasons.length;
 
-        response.results[index] = {
+        tvShows.results[index] = {
           title: value.name,
           release_date: first_air_date,
           backdrop_path,
@@ -226,12 +263,15 @@ class Call {
           ...value,
         };
       });
-      this.results = response;
-      resolve(response);
+      this.results = tvShows;
+      resolve(tvShows);
     });
     return response_1;
   }
-
+  /**
+   *
+   * @returns la pagina successiva
+   */
   async nextPage() {
     if (this.path !== "") {
       this.pages++;
@@ -239,6 +279,10 @@ class Call {
       return await this.makeCall(this.path);
     }
   }
+  /**
+   *
+   * @returns la pagina precedente
+   */
   async prevPage() {
     if (this.path !== "") {
       this.pages--;
@@ -246,6 +290,11 @@ class Call {
       return await this.makeCall(this.path);
     }
   }
+  /**
+   * VAI ALLA PAGINA PASSATA
+   * @param {Number} number
+   * @returns
+   */
   async toPage(number) {
     if (this.path !== "") {
       this.pages = number;
@@ -253,6 +302,14 @@ class Call {
       return await this.makeCall(this.path);
     }
   }
+
+  /**
+   *
+   * @param {Number = 0} [index] Quale immagine dell'array prendere
+   * @param {NumberSize = 4} [numberSize] OPZIONALE grandezza dell'immagine nell'array di ["w92", "w154", "w185", "w342", "w500", "w780", "original"]
+   * @param {Array} poster_path array dove si trova l'indicazione della img
+   * @returns {String} Link completo all'immagine
+   */
   getImage(index = 0, numberSize = 4, poster_path) {
     let path;
     if (poster_path) path = poster_path;
@@ -263,7 +320,11 @@ class Call {
 
     return result;
   }
-
+  /**
+   * DATA LA SIGLA DELLA NAZIONE TORNA LA SUA BANDIERA IN EMOJI UNICODE
+   * @param {*} country SIGLA IN ISO3066 NAZIONE
+   * @returns {String} UNICODE EMOJI FLAG
+   */
   getFlag(country) {
     if (!country) {
       country = "US";
@@ -272,13 +333,16 @@ class Call {
     // console.log(codePoints)
     return String.fromCodePoint(...codePoints);
   }
-
+  /**
+   *
+   * @param {*} genre ID DEL GENERE
+   * @returns {{ results: Object [], title: genre.name }} Ritorna ogetto con i titoli del genere selezionato
+   */
   async getByCategory(genre) {
-    let category = new Call({ language: "it-IT", adult: false, with_genres: genre });
-
+    this.addQuery({ with_genres: genre });
     let res = await new Promise((resolve) => {
-      category.makeCall("discover", this.type).then(async (res) => {
-        category.moreInformationAllMulti(res).then((value) => {
+      this.makeCall("discover", this.type).then(async (res) => {
+        this.moreInformationAllMulti(res).then((value) => {
           resolve(value);
           // this.category.push({ results: value.results, title: "Last Release" });
         });
@@ -286,7 +350,12 @@ class Call {
     }).then((value) => value);
     return res;
   }
-
+  /**
+   *
+   * @param {Number} id Movie/film ID
+   * @param {string[]} [path] OPZIONALE- endpoint
+   * @returns {Object[]} Ritorna l'id con più informazioni
+   */
   async getMoreInformation(id, path) {
     let type = "";
     if (path) type = path;
@@ -304,22 +373,25 @@ class Call {
     return response;
   }
 
-/**
- * 
- * @param {string} title Title of carousel
- * @param  {...string} path TMDB callback
- * @returns 
- */
-  async getArrayTitle(title,...path) {
-    return await this.makeCall(...path).then(async (res) => {
-     console.log(this.type)
-      return await this.moreInformationAllMulti(res).then(({ results }) => {
-        return { results, title: title };
-     });
-     
-   }).then((res) => {
-    return res
-    });
+  /**
+   * dato un titolo e la path ritorna un array di oggetti contenenti i risultati con il more information aggiunto
+   * @param {string} title Title of carousel
+   * @param  {...string} path TMDB callback
+   * @returns {Object {
+   * title: string
+   * results: Array}}
+   */
+  async getArrayTitle(title, ...path) {
+    return await this.makeCall(...path)
+      .then(async (res) => {
+        console.log(this.type);
+        return await this.moreInformationAllMulti(res).then(({ results }) => {
+          return { results, title };
+        });
+      })
+      .then((res) => {
+        return res;
+      });
   }
 }
 let count = 0;
